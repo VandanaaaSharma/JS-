61ac06f562d3040535373d6d68e616433c67fd51
Vandana <94068256+VandanaaaSharma@users.noreply.github.com>
2024-11-10 08:51:38 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=12344
+let accountEmail = "abcd@gmail.com"
+var accPssword="1234"
+accountcity="Jaipur"
+
+// accountId=23 not allowed for constant
+console.log(accountId)
+accountEmail="bnm@gmail.com"
+accPssword="2345"
+accountcity="Delhi"
+console.table([accountId, accountEmail, accPssword, accountcity])
