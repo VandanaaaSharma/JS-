145ab6c4996f51c3d38a7b0048a72fe8569742df
Vandana <94068256+VandanaaaSharma@users.noreply.github.com>
2024-11-10 17:12:09 +0000

    done

---
+++ basics/conversion.js
@@ -0,0 +1,20 @@
+// let score="33a"
+// console.log(typeof score)
+// let value=Number(score)
+// console.log(typeof value)
+// console.log(value)
+// let score=null
+// console.log(typeof score)
+// let value=Number(score)
+// console.log(typeof value)
+// console.log(value)
+// let score="33"
+// console.log(typeof score)
+// let value=String(score)
+// console.log(typeof value)
+// console.log(value)
+let score="a"
+console.log(typeof score)
+let value=Boolean(score)
+console.log(typeof value)
+console.log(value)
