f2946f42bc17303aa9754bed35316acdf6f84960
Vandana <94068256+VandanaaaSharma@users.noreply.github.com>
2024-11-11 07:51:50 +0000

    Done

---
+++ basics/StringNumber.js
@@ -0,0 +1,39 @@
+// const name= "Vandana"
+// const Age =23
+// // console.log(name + Age + "value")
+// console.log(`My name is ${name} and age is ${Age}`)
+const name = new String('Vandana')
+// console.log(name[0])
+// console.log(name[1])
+// console.log(name[2])
+// console.log(name[3])
+// console.log(name[4])
+// console.log(name[5])
+// console.log(name[6])
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.charAt(5))
+// console.log(name.indexOf('d'))
+// const newString = name.substring(0,4)
+// console.log(newString)
+// const newString = name.slice(-7,6)
+// console.log(newString)
+//************************************Number***************************************
+const score = 89
+const bal=new Number(10)
+// console.log(bal)
+// console.log(bal.toString().length)
+// console.log(bal.toFixed(4))
+const number= 123.46
+const number1= 23.897
+// console.log(number.toPrecision(4))
+// console.log(number1.toPrecision(4))
+// for commas
+const no = 100000000
+// console.log(no.toLocaleString()) // ac. to internatinal
+// console.log(no.toLocaleString('en-IN')) 
+/////////*************************Maths */
+console.log(Math.abs(-4));
+console.log(Math.round(4.6))
+console.log(Math.min(4,5,6,-4))
+console.log(Math.random())
