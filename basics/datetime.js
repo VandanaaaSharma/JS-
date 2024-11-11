7dd7bf3633c735a6d5ad0150bb68aac0bfaa5645
Vandana <94068256+VandanaaaSharma@users.noreply.github.com>
2024-11-11 11:10:56 +0000

    Done

---
+++ basics/datetime.js
@@ -0,0 +1,14 @@
+//Date
+let date = new Date()
+// console.log(date)
+// console.log(date.toString())
+// console.log(date.toDateString())
+// console.log(date.toLocaleString())
+// console.log(typeof date)
+let mydate=new Date(2024011)
+// console.log(mydate)
+// console.log(date.toLocaleString())
+let myTimestamp=Date.now()
+console.log(myTimestamp)
+console.log(Math.floor(Date.now()/1000))
+console.log
