4ca365cfadeb09aad6d0fc11d1e030252f781998
Vandana <94068256+VandanaaaSharma@users.noreply.github.com>
2024-11-11 16:54:35 +0000

    Done

--- basics/Arrays.js
+++ basics/Arrays.js
@@ -15,14 +15,22 @@ const arr=[0,1,2,3,4]
 // // console.log(newarr)
 // console.log(typeof newarr)
 //Slice and splice
-console.log("A", arr1)
-const n1=arr1.slice(1,3)
-console.log(n1)
+// console.log("A", arr1)
+// const n1=arr1.slice(1,3)
+// console.log(n1)
 
-console.log("B", arr1)
-const n2=arr1.splice(1,3)
-console.log(n2)
+// console.log("B", arr1)
+// const n2=arr1.splice(1,3)
+// console.log(n2)
 
-console.log("C", arr1)
-console.log(n2)
+// console.log("C", arr1)
+// console.log(n2)
 
+
+// *********************** Array Function ************************
+const A = ["A", "B", "C"]
+const B = ["E","F","G"]
+//A.push(B) // Here B add in A as a 4 element
+console.log(A)  // to add bothe elemnt in this use concat
+const C = [...A, ...B] // This Method is called spread is also used for concat
+console.log(C)
