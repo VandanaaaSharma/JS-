850707ac04e52bea6d59efa6eb19a000155ea260
Vandana <94068256+VandanaaaSharma@users.noreply.github.com>
2024-11-13 16:54:24 +0000

    Done

---
+++ basics/IIFE.js
@@ -0,0 +1,8 @@
+// Immediately Invoked Function Expressions
+(function a(){
+    console.log("vdwh")
+})();// i parathesis ki help se humein ye nhi linkhan pdega a()
+
+((name)=>{
+    console.log(`connected${name}`);
+})("vandana")
