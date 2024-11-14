d7b6fd35291c7cb40d9f5cfe8c48d5f7224f7d3c
Vandana <94068256+VandanaaaSharma@users.noreply.github.com>
2024-11-14 11:26:11 +0000

    Done

--- basics/loop.js
+++ basics/loop.js
@@ -1,11 +1,11 @@
 // for loop
-for(let index=0;index<10;index++){
-    const element=index
-    if(element==5){
-       // console.log("5 is best")
-    }
-    // console.log(element)
-}
+// for(let index=0;index<10;index++){
+//     const element=index
+//     if(element==5){
+//        // console.log("5 is best")
+//     }
+//     // console.log(element)
+// }
 // Nested loop same as java
 // While loop
 // initializa
@@ -17,9 +17,15 @@ for(let index=0;index<10;index++){
 //    // console.log(`value ${index}`)
 //     index=index+2
 // }
-let Arr=["A","B","C"]
-let arr=0
-while(arr<Arr.length){
-    console.log(`value ${Arr[arr]}`)
-    arr=arr+1
+// let Arr=["A","B","C"]
+// let arr=0
+// while(arr<Arr.length){
+//     console.log(`value ${Arr[arr]}`)
+//     arr=arr+1
+// }
+let score=11
+do {
+    console.log(`score ${score}`);
+    score++;
 }
+while (score<=10);
