a11d3cf1a633cf830f42b201e77de37cc5bafef7
Vandana <94068256+VandanaaaSharma@users.noreply.github.com>
2024-11-14 11:19:23 +0000

    Done

---
+++ basics/loop.js
@@ -0,0 +1,25 @@
+// for loop
+for(let index=0;index<10;index++){
+    const element=index
+    if(element==5){
+       // console.log("5 is best")
+    }
+    // console.log(element)
+}
+// Nested loop same as java
+// While loop
+// initializa
+// while(condition){
+
+// }
+// let index=0
+// while(index<=10){
+//    // console.log(`value ${index}`)
+//     index=index+2
+// }
+let Arr=["A","B","C"]
+let arr=0
+while(arr<Arr.length){
+    console.log(`value ${Arr[arr]}`)
+    arr=arr+1
+}
