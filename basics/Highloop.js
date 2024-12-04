d426dd38b4096eb804d0d7697d2b5c6f8b139fba
Vandana <94068256+VandanaaaSharma@users.noreply.github.com>
2024-12-04 07:53:14 +0000

    Done

---
+++ basics/Highloop.js
@@ -0,0 +1,31 @@
+// // Array example
+// const arr = [1, 2, 3, 4, 5];
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// // Objects array example
+// const objects = [{ id: 1 }, { id: 2 }, { id: 3 }];
+// for (const obj of objects) {
+//     console.log(obj);
+// }
+
+// // Map example
+// const map = new Map();
+// map.set('IN', 'India');
+// map.set('US', 'United States');
+// for (const [key, value] of map) {
+//     console.log(`${key}: ${value}`);
+// }
+// const prog = ["js", "rb", "py"]
+// for(const key in prog){
+//     // console.log(key)
+//     console.log(prog[key])
+// }
+
+//*****************  for each
+const coding=["js", "cpp", "py", "rb"]
+coding.forEach( function (val){
+    console.log(val)
+
+})
